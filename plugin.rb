83d9376d02cce9f2d6a1435d10a299f6ef663df8
Julian Primetshofer <20190074@students.htl-perg.ac.at>
2022-03-02 12:28:50 +0100

    Printing all elements

--- plugin.rb
+++ plugin.rb
@@ -28,7 +28,7 @@ class AzureOAuth2Authenticator < ::Auth::OAuth2Authenticator
   def after_authenticate(auth)
     result = Auth::Result.new
 
-    if (info = auth['info'].present?)
+    if info = auth['info'].present?
       email = auth['info']['email']
       if email.present?
         result.email = email
@@ -36,8 +36,6 @@ class AzureOAuth2Authenticator < ::Auth::OAuth2Authenticator
       end
       result.username = email.split('@')[0]
       result.name = auth['info']['name']
-
-      puts variables
 
       if name.include? "."
         result.moderator = true
